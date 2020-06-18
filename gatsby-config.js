89d0f1e0839de2deaed3ff2138a88ddbfe1d7c08
swfz <sawafuji.09@gmail.com>
2020-06-19 03:44:11 +0900

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `TIL`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `swfz`,
+      summary: `Today I Leaned`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `til`,
+    siteUrl: `https://til.swfz.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `swfz`,
     },
   },
   plugins: [

94772644ad019ea3cb2f8cda116f29d699faedee
swfz <sawafuji.09@gmail.com>
2020-06-19 06:04:35 +0900

    date format

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,7 +61,7 @@ export const pageQuery = graphql`
             slug
           }
           frontmatter {
-            date(formatString: "MMMM DD, YYYY")
+            date(formatString: "YYYY-MM-DD")
             title
             description
           }

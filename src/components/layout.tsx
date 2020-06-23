358cfeb2d2aabaa6450feac18e5246a51179710a
swfz <sawafuji.09@gmail.com>
2020-06-24 00:53:21 +0900

    chore: 不要箇所の削除

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,54 +5,9 @@ import { rhythm, scale } from "../utils/typography"
 import Navigation from "../components/navi"
 
 const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  let header
-
-  if (location.pathname === rootPath) {
-    header = (
-      <h1
-        style={{
-          ...scale(1.5),
-          marginBottom: rhythm(1.5),
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <h2
-        className="title"
-        style={{
-          fontFamily: `Montserrat, sans-serif`,
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h2>
-    )
-  }
   return (
     <>
       <Navigation></Navigation>
-      <header>{header}</header>
       <main
         style={{
           marginLeft: `auto`,

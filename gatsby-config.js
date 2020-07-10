234ec5cea26ecaef77ba55ad2130a094309cff0a
swfz <sawafuji.09@gmail.com>
2020-07-11 01:07:20 +0900

    feat: plugin sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -153,6 +153,7 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline

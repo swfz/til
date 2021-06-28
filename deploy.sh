b5d923dc397eedccfc0f0d0c1b00766fd702dac0
swfz <sawafuji.09@gmail.com>
2021-06-29 01:14:26 +0900

    chore: print

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+echo $CACHED_COMMIT_REF
+echo $COMMIT_REF
+
 # 差分があると終了コード1
 git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF content/blog/entries/
 
 rc=$?
 
 if [ "$rc" = "1" ]; then
+  echo "content changed."
   CONTENT_CHANGED=true gatsby build
 else
+  echo "content not changed."
   CONTENT_CHANGED=false gatsby build
 fi

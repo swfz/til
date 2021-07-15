73b4f039030ccbbbffc380972537bd91b7d121a0
swfz <sawafuji.09@gmail.com>
2021-07-16 01:06:25 +0900

    feat: 初期設定ファイル

---
+++ src/components/__tests__/header.ts
@@ -0,0 +1,10 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import Bio from "../bio"
+
+describe("Bio", () => {
+  it("renders correctly", () => {
+    expect(true).toBe(true);
+  })
+})

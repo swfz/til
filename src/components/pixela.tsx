2618efe8d3324001f088d103556878e4b10983c8
swfz <sawafuji.09@gmail.com>
2021-10-14 17:22:19 +0000

    refactor: delete node fetch

--- src/components/pixela.tsx
+++ src/components/pixela.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react"
-import fetch from "node-fetch"
 import { tippy } from "@tippyjs/react"
 import "tippy.js/dist/tippy.css"
 import DOMPurify from "dompurify"

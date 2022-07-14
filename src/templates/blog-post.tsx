ac9ad53d06fe33bf5b09f88f338771e2f2de6c4e
swfz <sawafuji.09@gmail.com>
2022-07-14 19:14:12 +0000

    feat: 型エラーが出ないようにした

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,109 +1,128 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { Link, graphql, PageProps } from "gatsby"
 import kebabCase from "lodash/kebabCase"
+import isNil from "lodash/isNil"
 
 import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
-import { BlogPostBySlugQuery } from "../../types/graphql-types"
+import {
+  BlogPostBySlugQuery,
+  AllMarkdownQuery,
+} from "../../types/graphql-types"
 
-type Props = {
-  data: BlogPostBySlugQuery
+declare type ElementType<T> = T extends (infer U)[] ? U : never
+type PageContext = {
+  previous: ElementType<AllMarkdownQuery["allMarkdownRemark"]["edges"]>["node"]
+  next: ElementType<AllMarkdownQuery["allMarkdownRemark"]["edges"]>["node"]
 }
 
-const BlogPostTemplate: React.FC<Props> = ({ data, pageContext, location }) => {
+const BlogPostTemplate: React.FC<
+  PageProps<BlogPostBySlugQuery, PageContext>
+> = ({ data, pageContext }) => {
   const post = data.markdownRemark
   const relatedPosts = data.relatedMarkdownRemarks?.posts?.slice(0, 3)
   const { previous, next } = pageContext
 
-  return (
-    <>
-      <SEO
-        title={post.frontmatter.title}
-        description={post.frontmatter.description || post.excerpt}
-      />
-      <article>
-        <header>
-          <h1 className="title">{post.frontmatter.title}</h1>
-          <p
+  if (
+    !isNil(post) &&
+    !isNil(post.html) &&
+    !isNil(post.frontmatter) &&
+    !isNil(post.frontmatter.tags)
+  ) {
+    return (
+      <>
+        <SEO
+          title={post.frontmatter.title ?? ""}
+          description={post.frontmatter.description || post.excerpt}
+        />
+        <article>
+          <header>
+            <h1 className="title">{post.frontmatter.title}</h1>
+            <p
+              style={{
+                ...scale(-1 / 5),
+                display: `block`,
+                marginBottom: rhythm(1),
+              }}
+            >
+              {post.frontmatter.date}
+              <span className="tags">
+                {post.frontmatter.tags.map(tag => (
+                  <Link
+                    className="tag is-link is-light"
+                    key={tag}
+                    to={`/tags/${kebabCase(tag || "")}`}
+                  >
+                    {tag}
+                  </Link>
+                ))}
+              </span>
+            </p>
+          </header>
+          <hr
             style={{
-              ...scale(-1 / 5),
-              display: `block`,
+              marginTop: rhythm(1),
+            }}
+          />
+          <section dangerouslySetInnerHTML={{ __html: post.html }} />
+          <hr
+            style={{
               marginBottom: rhythm(1),
             }}
+          />
+        </article>
+
+        <div>
+          <h2>関連記事</h2>
+          <ul>
+            {relatedPosts?.map(relatedPost => {
+              return (
+                <li>
+                  <Link to={relatedPost?.fields?.slug ?? ""}>
+                    {relatedPost?.frontmatter?.title}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+          <hr
+            style={{
+              marginTop: rhythm(1),
+            }}
+          />
+        </div>
+
+        <nav>
+          <ul
+            style={{
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `space-between`,
+              listStyle: `none`,
+              padding: 0,
+            }}
           >
-            {post.frontmatter.date}
-            <span className="tags">
-              {post.frontmatter.tags.map(tag => (
-                <Link
-                  className="tag is-link is-light"
-                  key={tag}
-                  to={`/tags/${kebabCase(tag)}`}
-                >
-                  {tag}
+            <li>
+              {previous && (
+                <Link to={previous?.fields?.slug || ""} rel="prev">
+                  ← {previous?.frontmatter?.title}
                 </Link>
-              ))}
-            </span>
-          </p>
-        </header>
-        <hr
-          style={{
-            marginTop: rhythm(1),
-          }}
-        />
-        <section dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-      </article>
-
-      <div>
-        <h2>関連記事</h2>
-        <ul>
-          {relatedPosts?.map(p => {
-            return (
-              <li>
-                <Link to={p?.fields.slug}>{p?.frontmatter.title}</Link>
-              </li>
-            )
-          })}
-        </ul>
-        <hr
-          style={{
-            marginTop: rhythm(1),
-          }}
-        />
-      </div>
-
-      <nav>
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
-    </>
-  )
+              )}
+            </li>
+            <li>
+              {next && (
+                <Link to={next?.fields?.slug || ""} rel="next">
+                  {next?.frontmatter?.title} →
+                </Link>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </>
+    )
+  } else {
+    return <></>
+  }
 }
 
 export default BlogPostTemplate

f7f76b3674fcceacc8791be9af5bf6b6adee2af3
swfz <sawafuji.09@gmail.com>
2022-07-14 20:14:23 +0000

    refactor: Lineコンポーネントの型改善

--- src/components/line.tsx
+++ src/components/line.tsx
@@ -3,29 +3,33 @@ import React from "react"
 import kebabCase from "lodash/kebabCase"
 import { rhythm } from "../utils/typography"
 import { Link } from "gatsby"
+import { AllMarkdownQuery } from "../../types/graphql-types"
 
-const Line: React.FC = ({ node }) => {
+declare type ElementType<T> = T extends (infer U)[] ? U : never
+type MarkdownNode = ElementType<AllMarkdownQuery["allMarkdownRemark"]["edges"]>
+
+const Line: React.FC<MarkdownNode> = ({ node }) => {
   return (
     <article className="card">
       <header className="card-content">
-        <small>[{node.frontmatter.date}]</small>
+        <small>[{node?.frontmatter?.date}]</small>
         <h2
           className="subtitle"
           style={{
             marginBottom: rhythm(1 / 4),
           }}
         >
-          <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-            {node.frontmatter.title}
+          <Link style={{ boxShadow: `none` }} to={node?.fields?.slug || ""}>
+            {node?.frontmatter?.title}
           </Link>
         </h2>
         <span className="tags">
-          {node.frontmatter.tags.map(tag => (
+          {node?.frontmatter?.tags?.map(tag => (
             <Link
               className="tag is-link is-light"
               aria-label={"tag"}
               key={tag}
-              to={`/tags/${kebabCase(tag)}`}
+              to={`/tags/${kebabCase(tag || "")}`}
             >
               {tag}
             </Link>

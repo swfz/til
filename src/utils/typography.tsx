4e2a6a28947b2724f3ee271402d992bb9ef45c29
swfz <sawafuji.09@gmail.com>
2022-07-15 09:42:39 +0000

    refactor: type improvement in typography

--- src/utils/typography.tsx
+++ src/utils/typography.tsx
@@ -1,19 +1,16 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
+const typography = new Typography({
+  headerFontFamily: ["Noto Sans JP"],
+  bodyFontFamily: ["Noto Sans JP"],
+  googleFonts: [
+    {
+      name: "Noto+Sans+JP",
+      styles: ["400"],
+    }
+  ]
+})
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
-
-// Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
   typography.injectStyles()
 }

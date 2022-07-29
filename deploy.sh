1954267c67c4d8b49946a62e70c1cffd228870f8
swfz <sawafuji.09@gmail.com>
2022-07-29 20:54:22 +0000

    chore: change api endpoint

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-LATEST_DEPLOY_COMMIT_SHA=$(curl -X GET "https://api.cloudflare.com/client/v4/accounts/${CF_ACCOUNT_ID}/pages/projects/til" \
+LATEST_DEPLOY_COMMIT_SHA=$(curl -X GET "https://api.cloudflare.com/client/v4/accounts/${CF_ACCOUNT_ID}/pages/projects/til/deployments" \
   -H "Authorization: Bearer ${CF_API_KEY}" \
-  -H "Content-Type:application/json" | jq -rc '.result.latest_deployment.deployment_trigger.metadata.commit_hash')
+  -H "Content-Type:application/json" | jq -rc '.result[1].deployment_trigger.metadata.commit_hash')
 
 echo "latest commit sha: $LATEST_DEPLOY_COMMIT_SHA"
 echo "current commit sha: $CF_PAGES_COMMIT_SHA"

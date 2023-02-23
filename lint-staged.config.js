950ebd2197388f3f567ff92c56ee52887a89c8d6
swfz <sawafuji.09@gmail.com>
2023-02-23 18:49:10 +0900

    chore: lint系の設定を変更した

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   "**/*.md": ["textlint"],
   "**/*.{ts,tsx}": ["bash -c 'yarn typecheck'"],
+  "*.@(js|ts|tsx)": filenames => `yarn eslint -c .eslintrc.json ${filenames.join(" ")} | reviewdog -f=eslint -fail-on-error -diff='git diff --cached' -reporter local`,
 }

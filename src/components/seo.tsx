15f8540f1657bc1d35ce4e6cd6ce40f8c345df18
swfz <sawafuji.09@gmail.com>
2023-04-28 03:07:26 +0900

    fix: migrate

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -18,7 +18,7 @@ type Props = {
   children?: React.ReactNode
 }
 
-const SEO: React.FC<Props> = ({ description, meta, title, children }) => {
+const SEO: React.FC<Props> = ({ description, title, children }) => {
   const { site } = useStaticQuery(
     graphql`
       query Site {
@@ -39,55 +39,21 @@ const SEO: React.FC<Props> = ({ description, meta, title, children }) => {
 
   const decodedTitle = decodeURI(title || "")
   const ogpImage = `https://res.cloudinary.com/dss6ly6hy/image/upload/s--CZpmof8E--/c_limit,h_600,w_1200/co_rgb:C800D4,l_text:arial_30_bold_normal_left:${decodedTitle}/fl_layer_apply,g_center/til/til-ogp_xsuuux.jpg`
+  const titleTemplate = `%s | ${site.siteMetadata.title}`
 
   return (
     <>
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta=
-      {[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `article`,
-        },
-        {
-          property: `og:image`,
-          content: ogpImage,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary_large_image`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.social.twitter,
-        },
-        {
-          name: `twitter:image`,
-          content: ogpImage,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-        ...(meta || []),
-      ]}
+      <title>{titleTemplate}</title>
+      <meta name="description" content={metaDescription} />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={metaDescription} />
+      <meta name="og:type" content="article" />
+      <meta name="og:image" content={ogpImage} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={site.siteMetadata.social.twitter} />
+      <meta name="twitter:image" content={ogpImage} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={metaDescription} />
       {children}
     </>
   )

25be4fc69031d6d6bc52ebf4c0e8cc61cd169b2d
swfz <sawafuji.09@gmail.com>
2023-08-27 04:39:57 +0900

    fix: follow breaking change

--- src/components/__tests__/line.tsx
+++ src/components/__tests__/line.tsx
@@ -1,6 +1,6 @@
+import "@testing-library/jest-dom"
 import { render } from "@testing-library/react"
 import React from "react"
-import "@testing-library/jest-dom/extend-expect"
 import renderer from "react-test-renderer"
 
 import Line from "../line"

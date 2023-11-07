811cb281dee6950c28667182c781006c79b8979c
swfz <sawafuji.09@gmail.com>
2023-11-07 10:00:56 +0900

    feat: migrate tailwindcss layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -24,16 +24,17 @@ const Layout: React.FC<LayoutProps> = (props: LayoutProps) => {
       <Navigation></Navigation>
       <div className="flex flex-col md:flex-row">
         <div className="basis-0 md:basis-3/12"></div>
-        <div className="basis-6/12" style={{ padding: 0, background: "#FFFFFF" }}>
-          {props.children}
-        </div>
-        <div className="basis-2/12">
+
+        <div className="basis-6/12">{props.children}</div>
+
+        <div className="basis-2/12 bg-gray-100 p-1 md:p-2">
           <Bio></Bio>
           <hr />
           <Archive></Archive>
           <hr />
           <Pixela></Pixela>
         </div>
+
         <div className="grow"></div>
       </div>
       <Footer></Footer>

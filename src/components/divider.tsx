ad097ee80b30c2d64a690c2b4a55e10083f00d68
swfz <sawafuji.09@gmail.com>
2023-11-08 01:00:06 +0900

    feat: dividerの色変更

--- src/components/divider.tsx
+++ src/components/divider.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Divider = () => {
-  return <hr className="my-4 h-0.5 border-t border-gray-50" />
+  return <hr className="my-4 h-0.5 border-t border-zinc-50" />
 }
 
 export default Divider
